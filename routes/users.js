e30cc7cfa691fd1d8748abf20565972c71af6ee6
Syamsul FE <syamsul.amin@privyid.tech>
2022-05-20 19:34:05 +0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send(process.env.APP_NAME);
+});
+
+router.get('/me', function(req, res, next) {
+  res.json({
+    message: 'About Me Syamsul'
+  });
+});
+
+module.exports = router;
